425798897b3fa8b1ccb9de046c80ea2b3e2c1d1f
felerminoali <felerminoali@gmail.com>
2018-10-08 16:50:57 +0200

    security

---
+++ src/main/test/sgb/service/CustomUserDetailsServiceTest.java
@@ -0,0 +1,15 @@
+package sgb.service;
+
+import org.junit.Test;
+import sgb.domain.CustomUserDetails;
+
+import static org.junit.Assert.*;
+
+public class CustomUserDetailsServiceTest {
+
+    @Test
+    public void loadUserByUsername() {
+//        CustomUserDetails cs = (CustomUserDetails) new CustomUserDetailsService().loadUserByUsername("admin");
+//        assertEquals("admin", cs.getUsername());
+    }
+}
