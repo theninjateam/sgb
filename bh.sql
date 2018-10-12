315f13167b3d2e0bbcf70510d3b9a61a9a1f89a4
Bania Fonseca <bfonseca@unilurio.ac.mz>
2018-10-12 12:29:18 +0200

    i have modified the table tipoobra, see the made changes in the bh.sq

--- bh.sql
+++ bh.sql
@@ -1,138 +1,251 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : Herokudatabase
- Source Server Type    : PostgreSQL
- Source Server Version : 100005
- Source Host           : ec2-23-23-80-20.compute-1.amazonaws.com:5432
- Source Catalog        : d5b0hk16j5hm0n
- Source Schema         : public
-
- Target Server Type    : PostgreSQL
- Target Server Version : 100005
- File Encoding         : 65001
-
- Date: 04/10/2018 16:52:26
-*/
-
-
--- ----------------------------
--- Sequence structure for Tipo_de_Obra_IdTipoddd_seq
--- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."Tipo_de_Obra_IdTipoddd_seq";
-CREATE SEQUENCE "public"."Tipo_de_Obra_IdTipoddd_seq" 
-INCREMENT 1
-MINVALUE  1
-MAXVALUE 32767
-START 1
-CACHE 1;
-
--- ----------------------------
--- Table structure for AreaCientifca
--- ----------------------------
-DROP TABLE IF EXISTS "public"."AreaCientifca";
-CREATE TABLE "public"."AreaCientifca" (
-  "idAreaCientifica" int2 NOT NULL,
-  "Titulo" varchar(255) COLLATE "pg_catalog"."default"
+create table if not exists areacientifca
+(
+	"idAreaCientifica" smallint not null
+		constraint "AreaCientifca_pkey"
+			primary key,
+	titulo varchar(255)
 )
 ;
 
--- ----------------------------
--- Table structure for Livro
--- ----------------------------
-DROP TABLE IF EXISTS "public"."Livro";
-CREATE TABLE "public"."Livro" (
-  "Cota" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
-  "ISBN" varchar(255) COLLATE "pg_catalog"."default",
-  "Editora" varchar(255) COLLATE "pg_catalog"."default",
-  "Edicao" varchar(255) COLLATE "pg_catalog"."default",
-  "CodigoBarra" varchar(255) COLLATE "pg_catalog"."default"
+alter table areacientifca owner to postgres
+;
+
+create table if not exists formatocd
+(
+	idformato smallserial not null
+		constraint formatocd_pkey
+			primary key,
+	formato varchar(255)
 )
 ;
 
--- ----------------------------
--- Table structure for Obra
--- ----------------------------
-DROP TABLE IF EXISTS "public"."Obra";
-CREATE TABLE "public"."Obra" (
-  "Cota" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
-  "Registro" int4,
-  "NomeAutor" varchar(255) COLLATE "pg_catalog"."default",
-  "TituloObra" varchar(255) COLLATE "pg_catalog"."default",
-  "AreaCientifica" int2,
-  "LocalPublicacao" varchar(255) COLLATE "pg_catalog"."default",
-  "DataPublicacao" date,
-  "Idioma" varchar(255) COLLATE "pg_catalog"."default",
-  "Quantidade" int4,
-  "Tipo_Obra" int4
+alter table formatocd owner to postgres
+;
+
+create table if not exists item
+(
+	item_id integer not null
+		constraint item_pkey
+			primary key,
+	descricao varchar(255),
+	item varchar(255) not null
 )
 ;
 
--- ----------------------------
--- Table structure for Registro_de_Obra
--- ----------------------------
-DROP TABLE IF EXISTS "public"."Registro_de_Obra";
-CREATE TABLE "public"."Registro_de_Obra" (
-  "Cota" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
-  "Nome_Bibliotecario" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
-  "Data_Registro" date
+alter table item owner to postgres
+;
+
+create table if not exists role
+(
+	role_id integer not null
+		constraint role_pkey
+			primary key,
+	role varchar(255)
 )
 ;
 
--- ----------------------------
--- Table structure for Tipo_de_Obra
--- ----------------------------
-DROP TABLE IF EXISTS "public"."Tipo_de_Obra";
-CREATE TABLE "public"."Tipo_de_Obra" (
-  "Descricao" varchar(300)[] COLLATE "pg_catalog"."default" NOT NULL,
-  "IdTipo" int2 NOT NULL DEFAULT nextval('"Tipo_de_Obra_IdTipoddd_seq"'::regclass)
+alter table role owner to postgres
+;
+
+create table if not exists item_role
+(
+	item_id integer not null
+		constraint item_role_item_id_fkey
+			references item,
+	role_id integer not null
+		constraint item_role_role_id_fkey
+			references role,
+	constraint item_role_pkey
+		primary key (item_id, role_id)
 )
 ;
 
--- ----------------------------
--- Alter sequences owned by
--- ----------------------------
-ALTER SEQUENCE "public"."Tipo_de_Obra_IdTipoddd_seq"
-OWNED BY "public"."Tipo_de_Obra"."IdTipo";
-SELECT setval('"public"."Tipo_de_Obra_IdTipoddd_seq"', 3, false);
+alter table item_role owner to postgres
+;
 
--- ----------------------------
--- Primary Key structure for table AreaCientifca
--- ----------------------------
-ALTER TABLE "public"."AreaCientifca" ADD CONSTRAINT "AreaCientifca_pkey" PRIMARY KEY ("idAreaCientifica");
+create table if not exists roleitem
+(
+	item varchar(255) not null,
+	idrole integer not null,
+	constraint roleitem_pkey
+		primary key (item, idrole)
+)
+;
 
--- ----------------------------
--- Primary Key structure for table Livro
--- ----------------------------
-ALTER TABLE "public"."Livro" ADD CONSTRAINT "Livro_pkey" PRIMARY KEY ("Cota");
+alter table roleitem owner to postgres
+;
 
--- ----------------------------
--- Primary Key structure for table Obra
--- ----------------------------
-ALTER TABLE "public"."Obra" ADD CONSTRAINT "Obra_pkey" PRIMARY KEY ("Cota");
+create table if not exists tipoobra
+(
+	descricao varchar(300) not null,
+	idtipo smallserial not null
+		constraint "Tipo_de_Obra_pkey"
+			primary key
+)
+;
 
--- ----------------------------
--- Primary Key structure for table Registro_de_Obra
--- ----------------------------
-ALTER TABLE "public"."Registro_de_Obra" ADD CONSTRAINT "Registro_de_Obra_pkey" PRIMARY KEY ("Cota");
+alter table tipoobra owner to postgres
+;
 
--- ----------------------------
--- Primary Key structure for table Tipo_de_Obra
--- ----------------------------
-ALTER TABLE "public"."Tipo_de_Obra" ADD CONSTRAINT "Tipo_de_Obra_pkey" PRIMARY KEY ("IdTipo");
+create table if not exists obra
+(
+	cota varchar(255) not null
+		constraint "Obra_pkey"
+			primary key,
+	registro integer,
+	autores varchar(255),
+	titulo varchar(255),
+	areacientifica smallint
+		constraint "idAreacientifca"
+			references areacientifca,
+	localpublicacao varchar(255),
+	datapublicacao date,
+	idioma varchar(255),
+	quantidade integer,
+	tipoobra integer
+		constraint "idTipo"
+			references tipoobra
+)
+;
 
--- ----------------------------
--- Foreign Keys structure for table Livro
--- ----------------------------
-ALTER TABLE "public"."Livro" ADD CONSTRAINT "Cota" FOREIGN KEY ("Cota") REFERENCES "public"."Obra" ("Cota") ON DELETE CASCADE ON UPDATE CASCADE;
+alter table obra owner to postgres
+;
 
--- ----------------------------
--- Foreign Keys structure for table Obra
--- ----------------------------
-ALTER TABLE "public"."Obra" ADD CONSTRAINT "IdAreaCientifca" FOREIGN KEY ("AreaCientifica") REFERENCES "public"."AreaCientifca" ("idAreaCientifica") ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE "public"."Obra" ADD CONSTRAINT "IdTipo" FOREIGN KEY ("Tipo_Obra") REFERENCES "public"."Tipo_de_Obra" ("IdTipo") ON DELETE CASCADE ON UPDATE CASCADE;
+create table if not exists cd
+(
+	idcd varchar(255) not null
+		constraint cd_pkey
+			primary key
+		constraint idcd
+			references obra,
+	comprimento varchar(255) not null
+)
+;
 
--- ----------------------------
--- Foreign Keys structure for table Registro_de_Obra
--- ----------------------------
-ALTER TABLE "public"."Registro_de_Obra" ADD CONSTRAINT "Cota" FOREIGN KEY ("Cota") REFERENCES "public"."Obra" ("Cota") ON DELETE CASCADE ON UPDATE CASCADE;
+alter table cd owner to postgres
+;
+
+create table if not exists formato_cd
+(
+	idcd varchar(255) not null
+		constraint idcd
+			references cd,
+	idformato smallint not null
+		constraint idformato
+			references formatocd,
+	constraint formato_cd_pkey
+		primary key (idcd, idformato)
+)
+;
+
+alter table formato_cd owner to postgres
+;
+
+create table if not exists livro
+(
+	cota varchar(255) not null
+		constraint "Livro_pkey"
+			primary key
+		constraint cota
+			references obra
+				on update cascade on delete cascade,
+	isbn varchar(255),
+	editora varchar(255),
+	edicao varchar(255),
+	codigobarra varchar(255)
+)
+;
+
+alter table livro owner to postgres
+;
+
+create table if not exists registroobra
+(
+	cota varchar(255) not null
+		constraint "Registro_de_Obra_pkey"
+			primary key
+		constraint "Cota"
+			references obra
+				on update cascade on delete cascade,
+	bibliotecario varchar(255) not null,
+	registro date
+)
+;
+
+alter table registroobra owner to postgres
+;
+
+create table if not exists revista
+(
+	cota varchar(255) not null
+		constraint revista_pkey
+			primary key
+		constraint cota
+			references obra
+)
+;
+
+alter table revista owner to postgres
+;
+
+create table if not exists topico
+(
+	idtopico smallserial not null
+		constraint topico_pkey
+			primary key,
+	titulo varchar(255),
+	autores varchar(255)
+)
+;
+
+alter table topico owner to postgres
+;
+
+create table if not exists topico_revista
+(
+	idtopico smallint not null
+		constraint idtopico
+			references topico,
+	idrevista varchar(16) not null
+		constraint idrevista
+			references revista,
+	constraint topico_revista_pkey
+		primary key (idtopico, idrevista)
+)
+;
+
+alter table topico_revista owner to postgres
+;
+
+create table if not exists "user"
+(
+	user_id integer not null
+		constraint user_pkey
+			primary key,
+	active integer,
+	email varchar(255),
+	last_name varchar(255),
+	name varchar(255),
+	password varchar(255)
+)
+;
+
+alter table "user" owner to postgres
+;
+
+create table if not exists user_role
+(
+	user_id integer not null
+		constraint fk859n2jvi8ivhui0rl0esws6o
+			references "user",
+	role_id integer not null
+		constraint fka68196081fvovjhkek5m97n3y
+			references role,
+	constraint user_role_pkey
+		primary key (user_id, role_id)
+)
+;
+
+alter table user_role owner to postgres
+;
+
