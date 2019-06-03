eba04468e17d2955d8a6c9ede2630326b4d1b324
batalha98 <dinhobatalha2015@gmail.com>
2019-06-03 12:40:41 +0200

    fazendo push

---
+++ src/main/test/sgb/report/GerarRelatorioTest.java
@@ -0,0 +1,17 @@
+package sgb.report;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GerarRelatorioTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void createPdf() {
+    }
+}
